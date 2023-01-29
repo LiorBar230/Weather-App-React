9fcf0c4f80615e651c601d944335740007c13838
LiorBar230 <Liorbar230@gmail.com>
2023-01-29 14:18:28 -0500

    added git repo

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <a href="https://github.com/LiorBar230/Weather-App-React">
+        {" "}
+        Open source code
+      </a>
+      <span> by Lior Bar</span>
+    </div>
+  );
+}
